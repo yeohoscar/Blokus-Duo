ee10b0872ea751e1a1d5969e380124b44e9f0abe
Oscar Yeoh <oscar.yeoh@ucdconnect.ie>
2022-02-20 14:43:31 +0000

    Started Unit tests, finished block tests

---
+++ Test/BlockUnitTests.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class BlockUnitTests {
+    @Test
+    void testDeepCopy() {
+        Block original = new Block(1, 2);
+        Block copy = new Block(original);
+
+        assertEquals(original.getX(), copy.getX());
+        assertEquals(original.getY(), copy.getY());
+
+        if (original.equals(copy)) {
+            fail("not deep copy");
+        }
+    }
+
+    @Test
+    void testInvert() {
+        Block b = new Block(2, 3);
+
+        b.invertX();
+        assertEquals(-2, b.getX());
+
+        b.invertY();
+        assertEquals(-3, b.getY());
+
+        b.invertY();
+        assertEquals(3, b.getY());
+    }
+}
