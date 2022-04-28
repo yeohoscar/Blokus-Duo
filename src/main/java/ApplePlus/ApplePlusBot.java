785b0400f5e0fd97bdba0109e74da97397e9706f
Oscar Yeoh <oscar.yeoh@ucdconnect.ie>
2022-04-28 13:08:14 +0100

    Deleted some files from other branch

--- src/main/java/ApplePlus/ApplePlusBot.java
+++ src/main/java/ApplePlus/ApplePlusBot.java
@@ -20,4 +20,45 @@ public class ApplePlusBot extends SimpleBotPlayer {
 
         return super.makeMove(board);
     }
+
+    /**
+     * Iterates through bot's moves and gets the best move
+     *
+     * @param moves the bot's available moves
+     * @return optimal move
+     */
+    public Move getOptimalMove(ArrayList<Move> moves) {
+        int maxPoints = 0;
+        Move optimalMove = null;
+        for (Move move : moves) {
+            int points = gradeMove(move);
+            if (maxPoints < points) {
+                maxPoints = points;
+                optimalMove = move;
+            }
+        }
+        return optimalMove;
+    }
+
+    /**
+     * Calculates how good the move is and assigns it points based on how good the move is.
+     *
+     * @param move move to be graded
+     * @return the points the move got
+     */
+    public int gradeMove(Move move) {
+        return //Big Piece Points * weight based on stage of game
+               //+ Builder * weight based on optimisation through play testing
+               //+ Blocker * weight based on optimisation through play testing
+               //+ NumMoves through later boards * weight based on optimisation through play testing
+    }
+    
+    private int bigPiece() {
+
+    }
+
+    private int builder(Move move) {
+        Board possibleBoard = new Board(board);
+        possibleBoard.makeMove(move);
+    }
 }
